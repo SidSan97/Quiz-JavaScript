f4a5c74edbdcd57763f02664ab39a3e362f54a31
SidSan97 <sidnei1.8santiago@hotmail.com>
2022-03-06 00:23:07 -0300

    adicionando funcionalidade de double click e mais opcoes de resposta. Animaçao

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
 
-let titulo = document.querySelector('h1')
+let titulo     = document.querySelector('h1')
 let instrucoes = document.querySelector('#instrucoes')
-let aviso = document.querySelector('#aviso')
-//let respostaEsta = document.querySelector('#respostaEsta')
+let aviso      = document.querySelector('#aviso')
+let progresso  = document.querySelector('#progresso')
 let pontos = 0 // pontos para o placar
 let placar = 0 // placar
+
+// AUDIO
+let somAcerto   = document.querySelector('#somAcerto')
+let somErro     = document.querySelector('#somErro')
+let somAplausos = document.querySelector('#somAplausos')
 
 // PERGUNTA
 let numQuestao = document.querySelector('#numQuestao')
@@ -14,6 +19,7 @@ let pergunta   = document.querySelector('#pergunta')
 let a = document.querySelector('#a')
 let b = document.querySelector('#b')
 let c = document.querySelector('#c')
+let d = document.querySelector('#d')
 
 // article com a class questoes
 let articleQuestoes = document.querySelector('.questoes')
@@ -26,56 +32,102 @@ const q0 = {
     alternativaA : "Alternativa A",
     alternativaB : "Alternativa B",
     alternativaC : "Alternativa C",
+    alternativaD : "Alternativa D",
     correta      : "0",
 }
-
 const q1 = {
     numQuestao   : 1,
     pergunta     : "Boreal é o mesmo que...",
     alternativaA : "Sul",
     alternativaB : "Leste",
     alternativaC : "Norte",
+    alternativaD : "Oeste",
     correta      : "Norte",
 }
-
 const q2 = {
     numQuestao   : 2,
-    pergunta     : "Austral é o mesmo que...",
-    alternativaA : "Oeste",
-    alternativaB : "Sul",
-    alternativaC : "Norte",
-    correta      : "Sul",
+    pergunta     : "Qual é a capital do Brasil?",
+    alternativaA : "Rio de Janeiro",
+    alternativaB : "Brasília",
+    alternativaC : "Salvador",
+    alternativaD : "Lisboa",
+    correta      : "Brasília",
 }
-
 const q3 = {
     numQuestao   : 3,
-    pergunta     : "Nascente é o mesmo que...",
-    alternativaA : "Sul",
+    pergunta     : "Austral é o mesmo que...",
+    alternativaA : "Oeste",
     alternativaB : "Leste",
-    alternativaC : "Oeste",
-    correta      : "Leste",
+    alternativaC : "Norte",
+    alternativaD : "Sul",
+    correta      : "Sul",
 }
-
 const q4 = {
     numQuestao   : 4,
-    pergunta     : "Poente é o mesmo que...",
-    alternativaA : "Norte",
-    alternativaB : "Leste",
-    alternativaC : "Oeste",
-    correta      : "Oeste",
+    pergunta     : "A linha do Equador divide a Terra em...",
+    alternativaA : "Leste e Oeste",
+    alternativaB : "Norte e Sul",
+    alternativaC : "Verão e Inverno",
+    alternativaD : "Solstícios e Eclipses",
+    correta      : "Norte e Sul",
 }
-
 const q5 = {
     numQuestao   : 5,
+    pergunta     : "Nascente é o mesmo que...",
+    alternativaA : "Lado que o sol nasce",
+    alternativaB : "Abaixo do Equador",
+    alternativaC : "Lado que o sol se põe",
+    alternativaD : "Acima do Equador",
+    correta      : "Lado que o sol nasce",
+}
+const q6 = {
+    numQuestao   : 6,
+    pergunta     : "Qual é o clima predominante do Brasil?",
+    alternativaA : "Polar",
+    alternativaB : "Tropical",
+    alternativaC : "Desértico",
+    alternativaD : "Temperado do Norte",
+    correta      : "Tropical",
+}
+const q7 = {
+    numQuestao   : 7,
+    pergunta     : "Poente é o mesmo que...",
+    alternativaA : "Lado onde o sol se põe",
+    alternativaB : "Lado onde o sol nasce",
+    alternativaC : "Abaixo do Equador",
+    alternativaD : "Acima do Equador",
+    correta      : "Lado onde o sol se põe",
+}
+const q8 = {
+    numQuestao   : 8,
     pergunta     : "O Brasil se localiza em qual continente?",
     alternativaA : "África",
     alternativaB : "Europa",
-    alternativaC : "América",
+    alternativaC : "Oceania",
+    alternativaD : "América",
     correta      : "América",
+}
+const q9 = {
+    numQuestao   : 9,
+    pergunta     : "Qual é a única capital do Brasil cortada pela linha do Equador?",
+    alternativaA : "Belém",
+    alternativaB : "São Luís",
+    alternativaC : "Macapá",
+    alternativaD : "Boa Vista",
+    correta      : "Macapá",
+}
+const q10 = {
+    numQuestao   : 10,
+    pergunta     : "Considerando a extensão territorial o Brasil é o ...",
+    alternativaA : "3º maior",
+    alternativaB : "2º maior",
+    alternativaC : "4º maior",
+    alternativaD : "5º maior",
+    correta      : "5º maior",
 }
 
 // CONSTANTE COM UM ARRAY DE OBJETOS COM TODAS AS QUESTOES
-const questoes = [q0, q1, q2, q3, q4, q5]
+const questoes = [q0, q1, q2, q3, q4, q5, q6, q7, q8, q9, q10]
 
 let numero = document.querySelector('#numero')
 let total  = document.querySelector('#total')
@@ -92,11 +144,13 @@ pergunta.textContent   = q1.pergunta
 a.textContent = q1.alternativaA
 b.textContent = q1.alternativaB
 c.textContent = q1.alternativaC
+d.textContent = q1.alternativaD
 
 // CONFIGURAR O VALUE INICIAL DA 1a QUESTAO COMPLETA
 a.setAttribute('value', '1A')
 b.setAttribute('value', '1B')
 c.setAttribute('value', '1C')
+d.setAttribute('value', '1D')
 
 // PARA MONTAR AS PROXIMAS QUESTOES
 function proximaQuestao(nQuestao) {
@@ -106,21 +160,43 @@ function proximaQuestao(nQuestao) {
     a.textContent = questoes[nQuestao].alternativaA
     b.textContent = questoes[nQuestao].alternativaB
     c.textContent = questoes[nQuestao].alternativaC
+    d.textContent = questoes[nQuestao].alternativaD
     a.setAttribute('value', nQuestao+'A')
     b.setAttribute('value', nQuestao+'B')
     c.setAttribute('value', nQuestao+'C')
+    d.setAttribute('value', nQuestao+'D')
+    progresso.value = parseInt(progresso.value) + 1
+    //console.log(progresso.value)
 }
 
+// VERIFICAR DUPLO CLICK NAS ALTERNATIVAS
+alternativas.addEventListener('dblclick', () => {
+    //console.log('Duplo clique')
+    pontos -= 10 // tirar 10 pontos em caso de duplo click
+    if(numQuestao.value == 10 && pontos == 110) { pontos = 100 }
+})
+
 function bloquearAlternativas() {
-    a.classList.add('bloqueado')
-    b.classList.add('bloqueado')
-    c.classList.add('bloqueado')
+    alternativas.classList.add('bloqueado')
 }
 
 function desbloquearAlternativas() {
-    a.classList.remove('bloqueado')
-    b.classList.remove('bloqueado')
-    c.classList.remove('bloqueado')
+    alternativas.classList.remove('bloqueado')
+}
+
+function piscarNoAcerto() {
+    articleQuestoes.classList.remove('errou')
+    articleQuestoes.classList.add('acertou')
+}
+
+function piscarNoErro() {
+    articleQuestoes.classList.remove('acertou')
+    articleQuestoes.classList.add('errou')
+}
+
+function tirarPiscar() {
+    articleQuestoes.classList.remove('acertou')
+    articleQuestoes.classList.remove('errou')
 }
 
 function verificarSeAcertou(nQuestao, resposta) {
@@ -137,12 +213,20 @@ function verificarSeAcertou(nQuestao, resposta) {
     if(respostaEscolhida == certa) {
         //console.log("Acertou")
         //respostaEsta.textContent = "Correta 😊"
+        piscarNoAcerto()
+        somAcerto.play()
         pontos += 10 // pontos = pontos + 10
+        if(nQuestao.value == 1 && pontos == 20) { pontos = 10 }
     } else {
         //console.log("Errou!")
         //respostaEsta.textContent = "Errada 😢"
+        piscarNoErro()
+        somErro.play()
     }
-
+    setTimeout(() => {
+        tirarPiscar()
+    }, 150);
+    
     // atualizar placar
     placar = pontos
     instrucoes.textContent = "Pontos " + placar
@@ -151,7 +235,7 @@ function verificarSeAcertou(nQuestao, resposta) {
     bloquearAlternativas()
 
     setTimeout(function() {
-        //respostaEsta.textContent = '...'
+
         proxima = numeroDaQuestao+1
 
         if(proxima > totalDeQuestoes) {
@@ -160,34 +244,31 @@ function verificarSeAcertou(nQuestao, resposta) {
         } else {
             proximaQuestao(proxima)
         }
-    }, 5000)
+    }, 150)
     desbloquearAlternativas()
 }
 
 function fimDoJogo() {
-    instrucoes.textContent = "Fim de Jogo!"
-    numQuestao.textContent = ""
 
-    let pont = ''
-    pontos == 0 ? pont = 'ponto' : pont = 'pontos'
+    somAplausos.play()
 
-    pergunta.textContent   = "Você conseguiu " + pontos + " " + pont
+    let s = 's'
+    pontos == 0 ? s = '' : s = s
+    instrucoes.textContent = "Fim de Jogo! Você conseguiu " + pontos + " ponto"+ s
 
-    aviso.textContent = "Você conseguiu " + pontos + " " + pont
-
-    a.textContent = ""
-    b.textContent = ""
-    c.textContent = ""
-
-    a.setAttribute('value', '0')
-    b.setAttribute('value', '0')
-    c.setAttribute('value', '0')
+    instrucoes.classList.add('placar')
 
     // OCULTAR O ARTICLE DA QUESTAO
     articleQuestoes.style.display = 'none'
 
     setTimeout(function() {
         pontos = 0 // zerar placar
-        location.reload();
-    }, 2000)
+        //location.reload();
+        instrucoes.classList.remove('placar')
+        // REINICIAR O JOGO
+        articleQuestoes.style.display = 'block'
+        proximaQuestao(1)
+        instrucoes.textContent = 'Leia a questão e clique na resposta correta'
+    }, 8000)
+
 }
